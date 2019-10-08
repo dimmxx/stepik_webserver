e7fc9e263f5ebbf21ce5560b687c301e23da3f12
dimmxx <dimmxx@gmail.com>
2019-10-08 04:31:06 +0300

    mac 08-10-2019 simplified

--- src/main/java/servlets/AllRequestsServlet.java
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -1,39 +1,18 @@
 package servlets;
 
-import templater.PageGenerator;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AllRequestsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        Map<String, Object> pageVariables = createPageVariablesMap(request);
-
-        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+        response.getWriter().println(request.getParameter("value"));
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-
-
-    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
-
-        Map<String, Object> pageVariables = new HashMap<>();
-
-        pageVariables.put("value", request.getParameterMap().get("key")[0]);
-        return pageVariables;
-    }
-
 }
