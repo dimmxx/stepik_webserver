e1b87376ad85860e2f7366929ca83114be0cbb2c
dimmxx <dimmxx@gmail.com>
2019-10-15 22:03:29 +0300

    mac 15-10-2019

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,8 +10,9 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
 import servlets.SessionsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 import util.ClazzUtil;
 
 public class Main {
@@ -22,24 +23,17 @@ public class Main {
 
         AccountService accountService = new AccountService();
 
-        accountService.addNewUser(new UserProfile("admin"));
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
-        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
-
-
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("public_html");
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
 
         Server server = new Server(8080);
-        server.setHandler(handlers);
+        server.setHandler(context);
 
         server.start();
         logger.debug("server is started");
+        System.out.println("Server started");
         server.join();
         logger.debug("server is joined");
 
---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        UserProfile profile = new UserProfile(login, password, login + "@mail.com");
+        accountService.addNewUser(profile);
+        //accountService.addSession(req.getSession().getId(), profile);
+
+        resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("Status code: " + resp.getStatus() + "<br>");
+        resp.getWriter().println("User " + login + " is registered");
+
+    }
+}
